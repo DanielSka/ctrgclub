e5d540e6e43d7cebdedaa601e3b4b012a4b2423c
DanielSka <87310888+DanielSka@users.noreply.github.com>
2025-08-31 19:40:12 -0400

    Add files via upload

---
+++ ConnectorsWS/Controllers/ConctactController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContactService.Controllers
+{
+    public class ConctactController : Controller
+    {
+        //
+        // GET: /Conctact/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // POST api/values
+        public ActionResult Post(string myInputData)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Conctact/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Conctact/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Conctact/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Conctact/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Conctact/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Conctact/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Conctact/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ConnectorsWS/Scripts/JavaScript.js
@@ -0,0 +1,57 @@
+﻿function SubscribeMe() {
+    alert('SubscribeMe in scripts.');
+    var email = document.getElementById('SubscribeEmail').value;
+    if (email != "") {
+        alert(email);
+
+        $.ajax({
+            type: "POST", url: "http://localhost/SiteHelper/SiteHelper.svc/SubscribeEmail",
+            async: false,
+            data: email,
+            contentType: "text/plain",
+            //data: '{"email":"' + email + '"}',
+            //contentType: "application/json; charset=utf-8",
+            //data: JSON.stringify(data),
+            dataType: "text",
+            success: onSucess, error: onError
+        });
+
+        function onSucess(result) {
+            alert('Sucess.');
+            alert(result);
+        }
+        function onError(result) {
+            alert('Something wrong.');
+        }
+    }
+}
+
+function RegisterNewPerson() {
+    alert('Processing RegisterNewPerson.');
+    var firstName = document.getElementById('firstName').value;
+    var lastName = document.getElementById('lastName').value;
+    var email = document.getElementById('email').value;
+    var cellPhone = document.getElementById('cellPhone').value;
+    var homePhone = document.getElementById('homePhone').value;
+    var street = document.getElementById('street').value;
+    var city = document.getElementById('city').value;
+    var state = document.getElementById('state').value;
+    var zip = document.getElementById('zip').value;
+
+    $.ajax({
+        type: "POST", url: "http://localhost/SiteHelper/SiteHelper.svc/RegisterContact",
+        async: false,
+        data: '{"firstName":"' + firstName + '", "lastName":"' + lastName + '",  "email":"' + email + '", "cellPhone":"' + cellPhone + '","homePhone":"' + homePhone + '","street":"' + street + '","city":"' + city + '","state":"' + state + '","zip":"' + zip + '"}',
+        contentType: "application/json; charset=utf-8",
+        success: onSucess, error: onError
+    });
+
+    function onSucess(result) {
+        alert('Sucess.');
+        alert(result);
+    }
+    function onError(result) {
+        alert('Error.');
+        alert(result);
+    }
+}
